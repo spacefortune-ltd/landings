942bb977fc0c853591900d5fddf459b084b14fc2
DmitrijLosev <dmitrij@spacefortune.ltd>
2025-04-13 23:49:52 +0300

    desktop version

--- chicken/js/main.js
+++ chicken/js/main.js
@@ -1,21 +1,49 @@
 jQuery(function() {
     jQuery(function() {
         jQuery('.main .container').on('click', '.buttons .go-btn', function() {
-            jQuery('.game-field .chicken').addClass('jump');
-            if (jQuery('body').hasClass('moved1')) {
-                jQuery('body').removeClass('moved1').addClass('move2');
+            if (jQuery('body').hasClass('win1')) {
+                jQuery('body').removeClass('win1').addClass('move2');
                 setTimeout(()=>{
-                    jQuery('body').removeClass('move2').addClass('moved2');
+                    jQuery('body').removeClass('move2').addClass('win2');
+                    jQuery('.game-field .x20').addClass('turn');
+                    setTimeout(()=>{
+                        jQuery('.game-field .x20').attr('src', 'content-img/goldchick.webp');
+                    },200)
                 },300)
-            } else if (jQuery('body').hasClass('moved2')) {
-                jQuery('body').removeClass('moved2').addClass('move3');
-            } else if (jQuery('body').hasClass('moved3')) {
-                jQuery('body').removeClass('moved3').addClass('move4');
-            } else if (jQuery('body').hasClass('moved4')) {
-                jQuery('body').removeClass('moved4').addClass('move5');
+            } else if (jQuery('body').hasClass('win2')) {
+                jQuery('body').removeClass('win2').addClass('move3');
+                setTimeout(()=>{
+                    jQuery('body').removeClass('move3').addClass('win3');
+                    jQuery('.game-field .x30').addClass('turn');
+                    setTimeout(()=>{
+                        jQuery('.game-field .x30').attr('src', 'content-img/goldchick.webp');
+                    },200)
+                },300)
+            } else if (jQuery('body').hasClass('win3')) {
+                jQuery('body').removeClass('win3').addClass('move4');
+                jQuery('.game-field').scrollLeft(300);
+                setTimeout(()=>{
+                    jQuery('body').removeClass('move4').addClass('win4');
+                    jQuery('.game-field .x50').addClass('turn');
+                    setTimeout(()=>{
+                        jQuery('.game-field .x50').attr('src', 'content-img/goldchick.webp');
+                    },200)
+                },300)
+            } else if (jQuery('body').hasClass('win4')) {
+                jQuery('body').removeClass('win4').addClass('move5');
                 jQuery('.go-btn').prop('disabled', true);
+                setTimeout(()=>{
+                    jQuery('body').removeClass('move5').removeClass('win4').removeClass('win3').addClass('win5');
+                },300)
             } else {
                 jQuery('body').addClass('move1');
+                setTimeout(()=>{
+                    jQuery('body').removeClass('move1').addClass('win1');
+                    jQuery('.game-field .x10').addClass('turn');
+                    setTimeout(()=>{
+                        jQuery('.game-field .x10').attr('src', 'content-img/goldchick.webp');
+                    },200)
+                },300)
             }
         });
     });
