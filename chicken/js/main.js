d1927bb7ab5981c4118fd9289a1c65574efcc8cd
DmitrijSPF <dmitrij@spacefortune.ltd>
2025-04-14 16:28:02 +0300

    finish chicken

--- chicken/js/main.js
+++ chicken/js/main.js
@@ -3,6 +3,9 @@ jQuery(function() {
         jQuery('.main .container').on('click', '.buttons .go-btn', function() {
             if (jQuery('body').hasClass('win1')) {
                 jQuery('body').removeClass('win1').addClass('move2');
+                if (window.innerWidth <= 1000) {
+                    jQuery('.game-field').scrollLeft(200);
+                }
                 setTimeout(()=>{
                     jQuery('body').removeClass('move2').addClass('win2');
                     jQuery('.game-field .x20').addClass('turn');
@@ -12,6 +15,9 @@ jQuery(function() {
                 },300)
             } else if (jQuery('body').hasClass('win2')) {
                 jQuery('body').removeClass('win2').addClass('move3');
+                if (window.innerWidth <= 1000) {
+                    jQuery('.game-field').scrollLeft(300);
+                }
                 setTimeout(()=>{
                     jQuery('body').removeClass('move3').addClass('win3');
                     jQuery('.game-field .x30').addClass('turn');
@@ -21,7 +27,12 @@ jQuery(function() {
                 },300)
             } else if (jQuery('body').hasClass('win3')) {
                 jQuery('body').removeClass('win3').addClass('move4');
-                jQuery('.game-field').scrollLeft(300);
+                if (window.innerWidth <= 1000) {
+                    jQuery('.game-field').scrollLeft(400);
+                }
+                if (window.innerWidth > 1000) {
+                    jQuery('.game-field').scrollLeft(300);
+                }
                 setTimeout(()=>{
                     jQuery('body').removeClass('move4').addClass('win4');
                     jQuery('.game-field .x50').addClass('turn');
@@ -31,12 +42,21 @@ jQuery(function() {
                 },300)
             } else if (jQuery('body').hasClass('win4')) {
                 jQuery('body').removeClass('win4').addClass('move5');
+                if (window.innerWidth <= 1000) {
+                    jQuery('.game-field').scrollLeft(500);
+                }
                 jQuery('.go-btn').prop('disabled', true);
                 setTimeout(()=>{
                     jQuery('body').removeClass('move5').removeClass('win4').removeClass('win3').addClass('win5');
                 },300)
+                setTimeout(function() {
+                    jQuery('body').addClass('show');
+                }, 1500);
             } else {
                 jQuery('body').addClass('move1');
+                if (window.innerWidth <= 1000) {
+                    jQuery('.game-field').scrollLeft(100);
+                }
                 setTimeout(()=>{
                     jQuery('body').removeClass('move1').addClass('win1');
                     jQuery('.game-field .x10').addClass('turn');
@@ -47,98 +67,5 @@ jQuery(function() {
             }
         });
     });
-
-
-    /*document.getElementById('currentYear').textContent = "\u00A0" + new Date().getFullYear().toString() + "\u00A0";
-
-    window.addEventListener("load", () => {
-        document.querySelectorAll(".win").forEach((w) => w.classList.remove('hidden'));
-        document.querySelector(".tabs").classList.remove('hidden');
-    });
-
-    jQuery('.cards').on('click', '.card', function() {
-        var card = jQuery(this);
-
-        if(card.hasClass('scratched') || card.hasClass('scratching') || card.hasClass('disable')) {
-            return
-        }
-        card.addClass('scratching');
-        var video = card.find('.video').get(0);
-
-        if (jQuery('.tab.sound').hasClass('active')) {
-            playSound(bufferCache.scratch, 0.4);
-        }
-
-        if(click === 0) {
-            playSound(bufferCache.scratch, 0.4);
-            if (!musicSource) {
-                musicSource = playSound(bufferCache.music, 0.2, true);
-            }
-            jQuery('.tabs .tab').removeClass('active');
-            jQuery('.tab.sound').addClass('active');
-        }
-
-        ++click;
-
-        if (video) {
-            video.play();
-            setTimeout(function() {
-                if (jQuery('.tab.sound').hasClass('active')) {
-                    playSound(bufferCache[`scratch${click}`], 0.4);
-                }
-                card.removeClass('scratching');
-                card.addClass("scratched");
-                const cards = [...document.querySelectorAll(".card")];
-                if (cards.length > 0 && cards.every((w) => w.classList.contains('scratched'))) {
-                    setTimeout(function() {
-
-                        if (jQuery('.tab.sound').hasClass('active')) {
-                            playSound(bufferCache.cong, 0.4);
-                        }
-
-                        document.querySelector('body').classList.add('show');
-                    }, 1000);
-                }
-            }, 1500);
-        }
-    });
-
-    jQuery('.tabs').on('click', '.tab', function () {
-        jQuery('.tabs .tab').removeClass('active');
-        jQuery(this).addClass('active');
-
-        if (jQuery(this).hasClass('sound')) {
-            if (!musicSource) {
-                musicSource = playSound(bufferCache.music, 0.2, true);
-            }
-        } else {
-            if (musicSource) {
-                musicSource.stop();
-                musicSource = null;
-            }
-        }
-    });
-
-    jQuery('a').on('click', function(e) {
-
-        e.preventDefault();
-
-        localStorage.setItem("play", "true");
-
-        const checkInterval = setInterval(() => {
-            if (localStorage.getItem("play") === "true") {
-                clearInterval(checkInterval);
-            }
-        }, 50);
-
-        if (jQuery('.tab.sound').hasClass('active')) {
-            playSound(bufferCache.click, 0.4);
-        }
-        musicSource.stop();
-        musicSource = null;
-
-        window.location.href = 'https://trck.bestonlinecasino.club/click';
-    });*/
-    
 
 });
